346a6c14f48f2fa2cbaac1c1a3687ef96c564134
master <master@SMLAB408PC14_23>
2022-12-13 19:28:01 -0500

    Agregar archivos de proyecto.

---
+++ IvonneVegaS3/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IvonneVegaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "ivega";
+            string contraseña = "12345";
+            if (usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Registro(txtUsuario.Text));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña incorrectos", "Cerrar");
+            }
+
+        }
+    }
+}
