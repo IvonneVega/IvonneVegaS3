c8eadfc00b69569936ce6f6da68ce19f65010e75
master <master@SMLAB408PC14_23>
2022-12-13 19:30:42 -0500

    Version inicial del proyecto

--- IvonneVegaS3/Registro.xaml.cs
+++ IvonneVegaS3/Registro.xaml.cs
@@ -15,7 +15,7 @@ namespace IvonneVegaS3
         public Registro(string usuario)
         {
             InitializeComponent();
-            lblUsuario.Text = usuario;
+            lblUsuario.Text = usuario; 
         }
     }
 }
